b919e240297550eba7028c3c308ac9fde0c0ae34
Sadia Imran <164052169+codewithsadia@users.noreply.github.com>
2024-10-22 22:01:11 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import { access } from "fs";
+import { Container } from "postcss";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+        container: {
+          center: true,
+          padding: "15px"
+        },
+        colors:{
+accent: "#08E95E"
+        },
+        
+      },
+    },
+  plugins: [],
+};
+export default config;
